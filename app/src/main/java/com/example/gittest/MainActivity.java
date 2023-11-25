2eab6cf554d3ecb885823b217ca550c7b30d7043
Michi <sb4010q@gre.ac.uk>
2023-11-25 16:39:24 +0000

    added new code

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.gittest;
 
+import androidx.annotation.NonNull;
 import
 
 
         androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        ;
+    }
+
+    @Override
+    public void setVrModeEnabled(boolean enabled, @NonNull ComponentName requestedComponent) throws PackageManager.NameNotFoundException {
+        super.setVrModeEnabled(enabled, requestedComponent);
     }
 }
